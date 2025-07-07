69fd9bcaa982fd3f8bf2edd08f01130a6d6d44c6
Subreena <subreena20@gmail.com>
2025-07-08 02:50:12 +0600

    trying again5

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# === Step 6: Run server ===
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+
